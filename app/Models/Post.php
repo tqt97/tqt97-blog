ba44cd90c6fa022e1a63b86e0c0bcf1b4bdbf6af
tqt97 <kutuanonline199@gmail.com>
2024-02-18 11:23:32 +0700

    add softdelete to post model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -7,10 +7,11 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     protected $fillable = [
         'user_id',
